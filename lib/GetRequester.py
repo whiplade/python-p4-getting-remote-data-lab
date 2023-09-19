5ec5cb8e973f08e3a94a181ae8b7e0b5cee2e0d0
whiplade <nmawira13@gmail.com>
2023-09-19 08:21:12 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        name_occupation = []
+        names = json.loads(self.get_response_body())
+        for name in names:
+            name_occupation.append(name)
+        return name_occupation
+    
